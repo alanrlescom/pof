95206d70135756de13b22833e7e909fa3818d087
Alan RL <alan.r.l.4018@gmail.com>
2021-12-13 02:13:53 -0600

    Detalles correo

--- restablecer.php
+++ restablecer.php
@@ -18,7 +18,7 @@ if (isset($_SESSION["email"])) {
     <title>Restablecer contraseña | POF</title>
     <link rel="shortcut icon" href="img/tw.png" type="image/x-icon" />
     <link rel="stylesheet" href="css/normalize.css" />
-    <link rel="stylesheet" href="css/login.alt.css" />
+    <link rel="stylesheet" href="css/login.css" />
 </head>
 
 <body>

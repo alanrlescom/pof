a12312f6f21bf27cc6de3b43f7923046c3cc55e4
Alan RL <alan.r.l.4018@gmail.com>
2021-12-21 20:49:39 -0600

    Arreglando fecha de entrega

--- rastreo.php
+++ rastreo.php
@@ -162,7 +162,7 @@ session_start();
                         <p class="text-lg font-bold">
                             Entregado
                         </p>
-                        <div class="flex flex-col" v-if="estado > ['1','3'].includes(data.envio.recoleccion) ? 4 : 3">
+                        <div class="flex flex-col" v-if="estado === 5">
                             <p>
                                 Entregado {{ fechaEntrega }}
                             </p>
